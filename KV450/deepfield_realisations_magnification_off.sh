27e586275aeae295a020d3865e8ba34f35026dd7
jlvdb <jlvdb@astro.uni-bonn.de>
2019-12-12 13:57:53 +0100

    removed an depreciated BPZ wrapper argument

--- KV450/deepfield_realisations_magnification_off.sh
+++ KV450/deepfield_realisations_magnification_off.sh
@@ -31,6 +31,7 @@ export BPZPATH=~/src/bpz-1.99.3
 # This is slightly less area then the DIR spec-z catalogues cover:
 # DEEP2: 0.82 sqdeg, VVDSf02: 0.51 sqdeg, zCOSMOS: 1.73 sqdeg
 echo "==> generate footprints for 100 survey dithers"
+test -e ${OUTROOT}/dither_footprint.txt && rm ${OUTROOT}/dither_footprint.txt
 mocks_generate_footprint \
     -b 35.0 40.0  6.0 22.9 \
     --survey DEEP2_dithers \
@@ -170,7 +171,6 @@ for survey in DEEP2 VVDSf02 zCOSMOS; do
                 vhs_j_obserr \
                 vhs_h_obserr \
                 vhs_ks_obserr \
-            --z-true z_cgal_v \
             --z-min 0.06674 \
             --z-max 1.42667 \
             --templates CWWSB_capak \
