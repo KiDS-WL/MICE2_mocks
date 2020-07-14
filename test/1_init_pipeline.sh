f89d4fd592004d00dd03a3c04e70c6944a99162e
jlvdb <jlvdb@astro.uni-bonn.de>
2020-07-14 19:21:27 +0200

    added test case for full Flagship simulation

--- test/1_init_pipeline.sh
+++ test/1_init_pipeline.sh
@@ -3,21 +3,21 @@
 if [ "$1" == "deep" ]
 then
     table="deep"
+    config=MICE2.toml
+    data=/net/home/fohlen12/jlvdb/DATA/MICE2_KV450/MICE2_${table}_uBgVrRciIcYJHKs_shapes_halos_WL.fits
+elif [ "$1" == "FS" ]
+then
+    table="FS"
+    config=Flagship.toml
+    data=/net/home/fohlen12/jlvdb/DATA/Flagship_KiDS/Flagship_v1-8-4_deep_ugrizYJHKs_shapes_halos_WL.fits
 else
     table="256th"
+    config=MICE2.toml
+    data=/net/home/fohlen12/jlvdb/DATA/MICE2_KV450/MICE2_${table}_uBgVrRciIcYJHKs_shapes_halos_WL.fits
 fi
 
-if [ "$(whoami)" == "janluca" ]
-then
-    ../scripts/mocks_init_pipeline \
-        /home/janluca/dev/MICE2_${table}_uBgVrRciIcYJHKs_shapes_halos_WL \
-        -i /home/janluca/dev/MICE2_${table}_uBgVrRciIcYJHKs_shapes_halos_WL.fits \
-        -c MICE2.toml \
-        --purge
-else
-    ../scripts/mocks_init_pipeline \
-        /net/home/fohlen12/jlvdb/DATA/MICE2_test_memmap_${table} \
-        -i /net/home/fohlen12/jlvdb/DATA/MICE2_KV450/MICE2_${table}_uBgVrRciIcYJHKs_shapes_halos_WL.fits \
-        -c MICE2.toml \
-        --purge
-fi
+../scripts/mocks_init_pipeline \
+    /net/home/fohlen12/jlvdb/DATA/MICE2_test_memmap_${table} \
+    -i $data \
+    -c $config \
+    --purge
