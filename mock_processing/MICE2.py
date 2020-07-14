9230d5ba253fd645411270ffbc60a4d0677fd6ee
jlvdb <jlvdb@astro.uni-bonn.de>
2020-07-14 18:29:42 +0200

    added an evolution correction wrapper for parallel execution

--- mock_processing/MICE2.py
+++ mock_processing/MICE2.py
@@ -1,17 +1,17 @@
 import numpy as np
 
 
-def evolution_correction(mag, redshift):
+def evolution_correction(redshift,mag):
     """
     Evolution correction as described in the official MICE2 manual
     https://www.dropbox.com/s/0ffa8e7463n8h1q/README_MICECAT_v2.0_for_new_CosmoHub.pdf?dl=0
 
     Parameters
     ----------
-    mag : array_like
-        Uncorrected model magnitudes.
     redshift : array_like
         True galaxy redshift (z_cgal).
+    mag : array_like
+        Uncorrected model magnitudes.
 
     Returns
     -------
@@ -19,3 +19,24 @@ def evolution_correction(mag, redshift):
         Evolution corrected model magnitudes.
     """
     return mag - 0.8 * (np.arctan(1.5 * redshift) - 0.1489)
+
+
+def evolution_correction_wrapped(redshift, *mags):
+    """
+    Wrapper for evolution_correction() to compute the evolution correction for
+    a set of magnitudes simultaneously.
+
+    Parameters:
+    -----------
+    redshift : array_like
+        True galaxy redshift (z_cgal).
+    *mags : array_like
+        Series of model magnitudes.
+
+    Returns
+    -------
+    mag_evos : tuple of array_like
+        Series of evolution corrected model magnitudes (matching input order).
+    """
+    mag_evos = tuple(evolution_correction(redshift, mag) for mag in mags)
+    return mag_evos
