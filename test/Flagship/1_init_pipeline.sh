8dbe14bb2b77f333ec49356f9f8feefdd866f868
jlvdb <jlvdb@astro.uni-bonn.de>
2020-07-16 15:00:38 +0200

    changed Flagship test input file path

--- test/Flagship/1_init_pipeline.sh
+++ test/Flagship/1_init_pipeline.sh
@@ -2,6 +2,6 @@
 
 ../../scripts/mocks_init_pipeline \
     /net/home/fohlen12/jlvdb/DATA/Flagship_test_memmap \
-    -i /net/home/fohlen12/jlvdb/DATA/Flagship_KiDS/Flagship_v1-8-4_deep_ugrizYJHKs_shapes_halos_WL.fits \
+    -i /net/home/fohlen12/jlvdb/DATA/Flagship_downloads/Flagship_subset.fits \
     -c Flagship.toml \
     --purge
