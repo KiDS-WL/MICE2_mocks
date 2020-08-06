45cea345f0dc25a6b46b3cbd3d473763372df4c0
jlvdb <jlvdb@astro.uni-bonn.de>
2020-08-06 21:52:15 +0200

    added sample selection testing scripts

---
+++ test/MICE2/9_samples.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [ "$1" == "deep" ]
+then
+    table="deep"
+elif [ "$1" == "test" ]
+then
+    table="256th"
+else
+    echo "ERROR: invalid setup \"$1\", must be \"test\" or \"deep\""
+    exit 1;
+fi
+
+for sample in KiDS 2dFLenS GAMA SDSS
+do
+    ../../scripts/mocks_select_sample \
+        /net/home/fohlen12/jlvdb/DATA/MICE2_test_memmap_${table} \
+        --sample $sample \
+        -c ${sample}.toml
+done
--- test/MICE2/get_fits.sh
+++ test/MICE2/get_fits.sh
@@ -11,9 +11,32 @@ else
     exit 1;
 fi
 
+footprint='position/ra/obs >= 40.0 AND position/ra/obs < 45.0 AND position/dec/obs >= 10.0 AND position/dec/obs < 15.0'
+
 ../../scripts/mocks_datastore_query \
     /net/home/fohlen12/jlvdb/DATA/MICE2_test_memmap_${table} --verify \
     -o ~/TEST/MICE2_query_${table}.fits \
-    -q "position/ra/obs >= 40.0 AND position/ra/obs < 45.0 AND position/dec/obs >= 10.0 AND position/dec/obs < 15.0"
+    -q "$footprint"
 
 # topcat ~/TEST/MICE2_query_${table}.fits ~/TEST/MICE2_query_reference.fits
+
+../../scripts/mocks_datastore_query \
+    /net/home/fohlen12/jlvdb/DATA/MICE2_test_memmap_${table} \
+    -o ~/TEST/MICE2_query_${table}_KiDS.fits \
+    -q "${footprint}"' AND samples/KiDS & 1'
+../../scripts/mocks_datastore_query \
+    /net/home/fohlen12/jlvdb/DATA/MICE2_test_memmap_${table} \
+    -o ~/TEST/MICE2_query_${table}_2dFLenS.fits \
+    -q "${footprint}"' AND samples/2dFLenS & 1'
+../../scripts/mocks_datastore_query \
+    /net/home/fohlen12/jlvdb/DATA/MICE2_test_memmap_${table} \
+    -o ~/TEST/MICE2_query_${table}_GAMA.fits \
+    -q "${footprint}"' AND samples/GAMA & 1'
+../../scripts/mocks_datastore_query \
+    /net/home/fohlen12/jlvdb/DATA/MICE2_test_memmap_${table} \
+    -o ~/TEST/MICE2_query_${table}_SDSS.fits \
+    -q "${footprint}"' AND samples/SDSS & 1'
+../../scripts/mocks_datastore_query \
+    /net/home/fohlen12/jlvdb/DATA/MICE2_test_memmap_${table} \
+    -o ~/TEST/MICE2_query_${table}_BOSS.fits \
+    -q "${footprint}"' AND samples/SDSS & 12'
