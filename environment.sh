b63080717f73e3daa283669d7bd708e2749bda82
Jan Luca van den Busch <jl.vdb@gmx.de>
2021-09-12 11:52:29 +0200

    added environment script

---
+++ environment.sh
@@ -0,0 +1,9 @@
+export PATH=${HOME}/src/table_tools:$PATH
+export PATH=${HOME}/src/stomp_tools:$PATH
+export PATH=${HOME}/src/MICE2_mocks/pipeline:$PATH
+export PATH=${HOME}/src/MICE2_mocks/plotting:$PATH
+
+export PYTHONPATH=${HOME}/src:$PYTHONPATH
+export PYTHONPATH=${HOME}/src/table_tools::$PYTHONPATH
+export PYTHONPATH=${HOME}/src/stomp_tools:$PYTHONPATH
+export PYTHONPATH=${HOME}/src/MICE2_mocks:$PYTHONPATH
