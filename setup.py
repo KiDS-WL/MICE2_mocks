2d2b386d869036facf3d96b2de61459e92eaf45b
jlvdb <jlvdb@astro.rub.de>
2022-03-23 15:16:06 +0100

    fixed the meta data

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ scripts = [
     os.path.join("bin", fname) for fname in os.listdir("bin")]
 
 setuptools.setup(
-    name="astropandas",
+    name="MICE2_mocks",
     version="1.0",
     author="Jan Luca van den Busch",
-    description="Tools to expand on pandas functionality for astronomical operations.",
+    description="This Repository provides code to create galaxy mock catalogues based on MICE galaxy catalogues.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jlvdb/astropandas",
+    url="https://github.com/KiDS-WL/MICE2_mocks",
     packages=setuptools.find_packages(),
     include_package_data=True,
     scripts=scripts,
